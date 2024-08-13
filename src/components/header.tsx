abc2b700289e599e75e926659bab581c9db34f8b
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-08-13 14:16:53 -0300

    feat: add CartSidebar component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,17 @@
-import { Logo } from "@/components/logo"
-import { ThemeToggle } from "@/components/theme-toogle"
+import { Logo } from "@/components/logo";
+import { ThemeToggle } from "@/components/theme-toogle";
+import { CartSidebar } from "@/components/cart/sidebar";
 
 export const Header = () => {
-    return (
-        <header className="flex justify-between items-center my-5 mx-3">
-            <div className="flex items-center gap-5">
-                <Logo />
-                <ThemeToggle />
-            </div>
-            <div className="flex items-center gap-5">
-                ...
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="flex justify-between items-center my-5 mx-3">
+      <div className="flex items-center gap-5">
+        <Logo />
+        <ThemeToggle />
+      </div>
+      <div className="flex items-center gap-5">
+        <CartSidebar />
+      </div>
+    </header>
+  );
+};
