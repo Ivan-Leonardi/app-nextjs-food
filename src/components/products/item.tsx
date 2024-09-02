3a7dc17361884489e688d22b4015637ac635804e
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-09-01 22:13:19 -0300

    feat: add components ProductItem and ProductEmpty

---
+++ src/components/products/item.tsx
@@ -0,0 +1,7 @@
+export const ProductItem = () => {
+    return (
+        <div>
+            ...
+        </div>
+    )
+}
--- src/components/products/tab.tsx
+++ src/components/products/tab.tsx
@@ -1,18 +1,64 @@
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { getAllProducts } from "@/services/product";
+import { Product } from "@/types/product";
+import { ProductEmpty } from "@/components/products/empty";
+import { ProductItem } from "./item";
 
-export const ProductsTab = () => {
+type Tab = {
+  title: string;
+  value: string;
+  products: Product[];
+};
+
+export const ProductsTab = async () => {
+  const products = await getAllProducts();
+
+  const tabs: Tab[] = [
+    {
+      title: "Sushi",
+      value: "sushi",
+      products: products.filter(item => item.category === 'sushi')
+    },
+    {
+      title: "Temaki",
+      value: "temaki",
+      products: products.filter(item => item.category === 'temaki')
+    },
+    {
+      title: "Combinados",
+      value: "pack",
+      products: products.filter(item => item.category === 'pack')
+    },
+    {
+      title: "Bebidas",
+      value: "beverage",
+      products: products.filter(item => item.category === 'beverage')
+    },
+  ];
+
   return (
-    <Tabs defaultValue="tab-1">
+    <Tabs defaultValue="sushi">
       <TabsList className="flex">
-        <TabsTrigger className="flex-1" value="tab-1">
-          Tab-1
-        </TabsTrigger>
-        <TabsTrigger className="flex-1" value="tab-2">
-          Tab-2
-        </TabsTrigger>
+        {tabs.map((item) => (
+          <TabsTrigger key={item.value} value={item.value} className="flex-1">
+            {item.title}
+          </TabsTrigger>
+        ))}
       </TabsList>
-      <TabsContent value="tab-1" className="mt-6">Conteudo 1</TabsContent>
-      <TabsContent value="tab-2" className="mt-6">Conteudo 2</TabsContent>
+      {tabs.map((item) => (
+        <TabsContent value={item.value} className="mt-6">
+          {item.products.length > 0 && 
+            <div className="grid gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+              {item.products.map(product => (
+                <ProductItem key={product.id} item={product} />
+              ))}
+            </div>
+          }
+          {item.products.length === 0 && 
+            <ProductEmpty />
+          }
+        </TabsContent>
+      ))}
     </Tabs>
   );
 };
