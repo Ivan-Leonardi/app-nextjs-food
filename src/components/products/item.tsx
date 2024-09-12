7abb355963703b4646390fe2d8b8bd362ae58ac6
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-09-11 23:03:33 -0300

    feat: add hook cart-stores

--- src/components/products/item.tsx
+++ src/components/products/item.tsx
@@ -1,25 +1,27 @@
-"use client"
+"use client";
 
 import { Product } from "@/types/product";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
-
+import { useCartStore } from "@/stores/cart-store";
 
 type Props = {
   item: Product;
 };
 
 export const ProductItem = ({ item }: Props) => {
-    const { toast } = useToast();
+  const { toast } = useToast();
+  const { upsertCartItem } = useCartStore((state) => state);
 
-    const handleAddButton = () => {
-        toast({
-            title: 'Adicionado ao carrinho!',
-            description: item.name,
-            action: <ToastAction altText="fechar">Fechar</ToastAction>       
-        })
-    }
+  const handleAddButton = () => {
+    upsertCartItem(item, 1);
+    toast({
+      title: "Adicionado ao carrinho!",
+      description: item.name,
+      action: <ToastAction altText="fechar">Fechar</ToastAction>,
+    });
+  };
 
   return (
     <div>
@@ -33,7 +35,9 @@ export const ProductItem = ({ item }: Props) => {
       <div className="mt-3 flex flex-col gap-2">
         <p className="text-lg">{item.name}</p>
         <p className="text-sm opacity-50">R$ {item.price.toFixed(2)}</p>
-        <Button variant="outline" onClick={handleAddButton}>Adicionar</Button>
+        <Button variant="outline" onClick={handleAddButton}>
+          Adicionar
+        </Button>
       </div>
     </div>
   );
